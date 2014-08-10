9a220bf1c298820472ff015b73cc1ac467ac4065
Gnzlt <igonxz@hotmail.com>
2014-08-10 23:33:00 +0200

    codekata Bowling Game

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using codekataBowlingGame;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void ExampleScore()
+        {
+            BowlingGame game = new BowlingGame();
+
+            game.roll(1);
+            game.roll(4);
+            game.roll(4);
+            game.roll(5);
+            game.roll(6);
+            game.roll(4);
+            game.roll(5);
+            game.roll(5);
+            game.roll(10);
+            game.roll(0);
+            game.roll(1);
+            game.roll(7);
+            game.roll(3);
+            game.roll(6);
+            game.roll(4);
+            game.roll(10);
+            game.roll(2);
+            game.roll(8);
+            game.roll(6);
+
+            Assert.AreEqual(133, game.score());
+        }
+
+        [TestMethod]
+        public void PerfectGame()
+        {
+            BowlingGame game = new BowlingGame();
+
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+            game.roll(10);
+
+            Assert.AreEqual(300, game.score());
+        }
+
+        [TestMethod]
+        public void GutterGame()
+        {
+            BowlingGame game = new BowlingGame();
+
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+            game.roll(0);
+
+            Assert.AreEqual(0, game.score());
+        }
+    }
+}
---
+++ codekataBowlingGame/BowlingGame.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codekataBowlingGame
+{
+    public class BowlingGame
+    {
+        private int Frame;
+        private int Roll;
+        private int Adder;
+        private int RollScore;
+        private int TotalScore;
+
+        public BowlingGame() // constructor
+        {
+            Frame = 1;
+            Roll = 1;
+            Adder = 0;
+            RollScore = 0;
+            TotalScore = 0;
+        }
+
+        public void roll(int pins) // throw of a bowling ball
+        {
+            TotalScore += pins;
+
+            if (Adder > 0)
+            {
+                TotalScore += pins;
+                Adder--;
+            }
+
+            switch (Roll)
+            {
+                case 1:
+                    if (Frame != 10)
+                    {
+                        if (pins == 10) // strike
+                        {
+                            Adder += 2;
+                            Frame++;
+                            Roll = 1;
+                            RollScore = 0;
+                        }
+                        else
+                        {
+                            RollScore += pins;
+                            Roll++;
+                        }
+                    }
+                    else
+                        Roll++;
+                    break;
+                case 2:
+                    if (Frame != 10)
+                    {
+                        if (RollScore + pins == 10) // spare
+                            Adder++;
+                        Frame++;
+                        Roll = 1;
+                        RollScore = 0;
+                    }
+                    else
+                        Roll++;
+                    break;
+                case 3:
+                    for (int i = 0; i < Adder; i++)
+                        TotalScore += pins;
+                    break;
+            }
+        }
+
+        public int score() // returns the total score
+        {
+            return TotalScore;
+        }
+    }
+}
